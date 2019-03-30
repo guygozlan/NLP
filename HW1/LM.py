03960968ec73f5ad0cad9ab9ffc1a670a3ddbc53
Guy Gozlan <ggozlan@apple.com>
2019-03-30 21:04:53 +0300

    HW1 - Done

--- HW1/LM.py
+++ HW1/LM.py
@@ -1,17 +1,32 @@
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, OrderedDict
 import numpy as np
 import pandas as pd
+import random
 
-CHARS_TO_REMOVE = "\r\n"
-START_SYMBOL = '<start>'
-END_SYMBOL = '<end>'
+###############################
+
+# Defint the base address of where the data is
+BASE_DATA_FILES_ADDRESS = '/Users/guygozlan/Documents/Private/Binthomi/NLP/git/NLP/HW1/data/'
+# Define the start and and symbols
+START_SYMBOL = '<s>'
+END_SYMBOL = '</s>'
+# Define the file names for each language
+LANGUAGE_LIST = ['en', 'es', 'fr', 'in', 'it', 'nl', 'pt', 'tl']
+# Train Vs. Test separation
+DATA_SPLIT = {'train': 0.09, 'test': 0.01 }
+# For Laplace smoothing - define the vocabulary size
+VOCAB_SIZE = 2000
+# Define weights
+WEIGHTS_LIST = [0.4,0.3,0.3]
+
+###############################
 
 # Module supports going over given input data, counts number of occurrences for each n-gram
 # And output module file according to defined format
 # start/end symbols will be treated as single char
 class LM:
     def __init__(self, data):
-        self.input_data    = data.translate(None, CHARS_TO_REMOVE)
+        self.input_data    = data.replace('\n','')
         self.start_symbol  = None
         self.end_symbol    = None
         self.start_n_gram  = None
@@ -45,6 +60,7 @@ class LM:
 
     # The function will create counters list according to n-grams defined
     # And output the results to output file according to format definition
+    # For unigram: We will smooth the data using Laplace
     def output_counters(self):
         self.counters_arr    = []
         self.n_gram_eval_arr = []
@@ -62,32 +78,19 @@ class LM:
                 for chars, val in occ_dict.items():
                     concatenate_chars = key+chars
                     prob = val/sum_of_occurences
-                    log_prob = np.log(prob)
+                    # Add One (Laplace) for unigram
+                    if (i==1): prob = (val+1)/(sum_of_occurences+VOCAB_SIZE)
+                    log_prob = np.log2(prob)
                     str+='{}\t{}\n'.format(concatenate_chars, log_prob)
             str+='\n'
         return(str)
 
-# For 1,2,3-Gram each element will contain n-chars, probability and log-probability
-class M3Gram:
-    w_i    = None
-    w_im1  = None
-    w_im2  = None
-    log2Px = None
-    Px     = None
-class M2Gram:
-    w_i    = None
-    w_im1  = None
-    log2Px = None
-    Px     = None
-class M2Gram:
-    w_i    = None
-    log2Px = None
-    Px     = None
-
 class Eval(object):
-    def __init__(self):
+    def __init__(self, input_data):
         self.start_symbol  = None
         self.end_symbol    = None
+        self.weights       = None
+        self.input_data    = input_data
 
     # This funtion counts the num of n-grams in model file. For out task - should be 3
     @staticmethod
@@ -104,72 +107,111 @@ class Eval(object):
         if (0 == str.find(self.end_symbol))  : return self.end_symbol
         return (str[0])
 
-    def create_3_gram_matrix(self,model_file):
-        self.m_3_gram = []
-        with open(model_file, 'r') as file:
-            if (file.readline() != '3-gram:\n'): raise ValueError('model_file is not is format. Could not find 3-gram')
-            for line in file:
-                if (line == '\n'): break
-                str, log2Px = line.split('\t')
-                m_3_gram_el = M3Gram()
-                m_3_gram_el.w_im2  = self.get_next_char(str)
-                m_3_gram_el.w_im1  = self.get_next_char(str[len(m_3_gram_el.w_im2):])
-                m_3_gram_el.w_i    = self.get_next_char(str[len(m_3_gram_el.w_im2)+len(m_3_gram_el.w_im1):])
-                m_3_gram_el.log2Px = float(log2Px)
-                m_3_gram_el.Px     = 2**m_3_gram_el.log2Px
-                self.m_3_gram.append(m_3_gram_el)
+    # Function creates 3 OrderedDict, 1 for each n-gram.
+    # Dict contains tuple:Probability_Val
+    def model_file_to_dict(self, model_file):
+        self.n_gram_dict = OrderedDict()
+        self.n_gram_dict['3-gram'] = OrderedDict()
+        self.n_gram_dict['2-gram'] = OrderedDict()
+        self.n_gram_dict['1-gram'] = OrderedDict()
 
-    def create_2_gram_matrix(self, model_file):
-        self.m_2_gram = []
-        with open(model_file, 'r') as file:
-            line = file.readline()
-            while (line != '2-gram:\n'): line = file.readline()
-            for line in file:
-                if (line == '\n'): break
-                str, log2Px = line.split('\t')
-                m_2_gram_el = M2Gram()
-                m_2_gram_el.w_im1  = self.get_next_char(str)
-                m_2_gram_el.w_i    = self.get_next_char(str[len(m_2_gram_el.w_im1):])
-                m_2_gram_el.log2Px = float(log2Px)
-                m_2_gram_el.Px     = 2 ** m_2_gram_el.log2Px
-                self.m_2_gram.append(m_2_gram_el)
+        with open(model_file, 'r') as f:
+            line = f.readline()
+            if (line != '3-gram:\n'): raise ValueError('Model file is not in correct format! Excpected 3-gram')
+            curr_n_gram = 3
+            for line in f:
+                if ('\n' == line): continue
+                if ('-gram:\n' in line):
+                    curr_n_gram = int(line.split('-')[0])
+                    continue
+                chars, logPr = line.replace('\n', '').split('\t')
+                self.n_gram_dict['{}-gram'.format(curr_n_gram)][chars] = 2**float(logPr)
 
-    def create_1_gram_matrix(self, model_file):
-        self.m_1_gram = []
-        with open(model_file, 'r') as file:
-            line = file.readline()
-            while (line != '1-gram:\n'): line = file.readline()
-            for line in file:
-                if (line == '\n'): break
-                str, log2Px = line.split('\t')
-                m_1_gram_el        = M2Gram()
-                m_1_gram_el.w_i    = self.get_next_char(str)
-                m_1_gram_el.log2Px = float(log2Px)
-                m_1_gram_el.Px     = 2 ** m_1_gram_el.log2Px
-                self.m_1_gram.append(m_1_gram_el)
+    # Calculates the P_interpolation according to given Lambda array for each 3-chars in model file
+    def calc_Pinterpolation(self, model_file):
+        self.model_file_to_dict(model_file)
+        self.interpolation_dict = OrderedDict()
+        for trigram_chars, trigram_Pr in self.n_gram_dict['3-gram'].items():
+            # Calculate the substrings to search in unigram and bigram models
+            bigram_str  = trigram_chars[len(self.get_next_char(trigram_chars)):]
+            unigram_str = bigram_str[len(self.get_next_char(bigram_str)):]
+            # Search the probability in the unigram and biagram models
+            bigram_Pr  = self.n_gram_dict['2-gram'][bigram_str]
+            unigram_Pr = self.n_gram_dict['1-gram'][unigram_str]
+            # Calculate the interpolation and store in self.interpolation_dict
+            Pinterp = self.weights[0] * trigram_Pr + self.weights[1] * bigram_Pr + self.weights[2] * unigram_Pr
+            self.interpolation_dict[''.join(trigram_chars)] = Pinterp
 
-    def get2GramElement(self, w_i, w_im1):
-        for el in self.m_2_gram:
-            if (el.w_i==w_i and el.w_im1==w_im1): return el
-        return None
+    # Going over the input char by char - creates the P_interp according to formula
+    # For cases where we have unfamiliar data - use Laplace for smoothing
+    # Then, calculate the preplexity according to formula
+    def calc_preplexity(self):
+        line = self.input_data
+        sum_log_P_interp = 0.0
+        while (len(line)>(1+len(self.end_symbol))):
+            t = ''
+            for j in range(3):
+                next_char = self.get_next_char(line)
+                t += next_char
+                line = line[len(next_char):]
+            try:
+                sum_log_P_interp += np.log2(self.interpolation_dict[t])
+            except:
+                # Add One (Laplace) for unigram: If string was not found: Pinterp=(1/VOCAB_SIZE)*unigram_weight + 0 + 0
+                sum_log_P_interp += np.log2(self.weights[2] * 1/VOCAB_SIZE)
+            # Skip empty end
+            if (line == self.start_symbol+self.end_symbol): break
 
-    def get1GramElement(self, w_i):
-        for el in self.m_2_gram:
-            if (el.w_i==w_i): return el
-        return None
+        # Calculate the preplexity
+        H = -1 * sum_log_P_interp/(len(self.input_data)-2)
+        Preplexity = 2**H
+        return(Preplexity)
 
-def preproc_csv_file(csv_file, start_symbol, end_symbol):
+############################
+
+# The function supports removing links (https://) and tags (@)
+# This will increase preplexity
+def clean_row_data(str):
+    ret = str
+    # Clear links
+    while ('https://' in ret):
+        ret = ret[:ret.find('https://')] + ' '.join(ret[ret.find('https://'):].split()[1:])
+    # Clear tags
+    while ('@' in ret):
+        ret = ret[:ret.find('@')] + ' '.join(ret[ret.find('@'):].split()[1:])
+
+    return(ret)
+
+# Assuming input is CSV file where column 'tweet_test' is the data
+# Combining all given data to one long string with seperations of start/end symbols over each line
+def preproc_csv_file(csv_file, start_symbol, end_symbol, clean_data = False):
     df = pd.read_csv(csv_file)
     data = start_symbol
     for line in df['tweet_text']:
+        if (len(line)==0): continue
+        if (clean_data):
+            line = clean_row_data(line)
         data+='{}{}{}'.format(line, end_symbol, start_symbol)
     data+=end_symbol
     return(data)
 
+# From each language in LANGUAGE_LIST
+# Split the data according to DATA_SPLIT
+# Creates training and test CSVs as and output
+def split_to_train_and_test_csvs():
+    for lang in LANGUAGE_LIST:
+        file_size = sum(1 for line in open(BASE_DATA_FILES_ADDRESS + lang + '.csv'))
+        for data_split in ['train', 'test']:
+            desired_data_size = int(file_size * DATA_SPLIT[data_split])
+            skip = sorted(random.sample(xrange(1,file_size+1), file_size-desired_data_size))
+            df = pd.read_csv(BASE_DATA_FILES_ADDRESS + lang + '.csv', skiprows=skip)
+            df.to_csv(BASE_DATA_FILES_ADDRESS + lang + '_' + data_split + '.csv')
+
 ###############################
 
+# Using corpus file - generate LM model file according to format
 def lm(corpus_file, model_file):
-    input_data = preproc_csv_file(corpus_file, START_SYMBOL, END_SYMBOL)
+    input_data = preproc_csv_file(corpus_file, START_SYMBOL, END_SYMBOL, clean_data=True)
 
     lm_m = LM(input_data)
     lm_m.start_symbol = START_SYMBOL
@@ -178,43 +220,41 @@ def lm(corpus_file, model_file):
     lm_m.stop_n_gram  = 4
 
     output = lm_m.output_counters()
-    print (output)
 
     with open(model_file, 'w+') as f_output:
         f_output.write(output)
 
+# Calculate preplexity with model and weights and applying on the input file
+# Return the preplexity
 def eval(input_file, model_file, weights):
-    eval_m = Eval()
+    input_data = preproc_csv_file(input_file, START_SYMBOL, END_SYMBOL)
+    eval_m = Eval(input_data)
     eval_m.start_symbol = START_SYMBOL
     eval_m.end_symbol   = END_SYMBOL
+    eval_m.weights      = weights
+    eval_m.calc_Pinterpolation(model_file)
+    preplexity = eval_m.calc_preplexity()
+    return(preplexity)
 
-    number_of_n_grams = eval_m.count_n_grams(model_file)
-    if (number_of_n_grams != len(weights)): raise ValueError('Number of weights does not match number of n_grams in the model file')
-    if (float(sum(weights)) != 1.0):        raise ValueError('Weights need to sum to exactly 1.0')
+# Pre-proccess the data into seperated CSVs
+# For each cell in DF: calculate preplexity
+# return DataFram results of all permutations
+def run():
+    # Split the data into test and training sets CSVs
+    split_to_train_and_test_csvs()
+    results = pd.DataFrame(columns=LANGUAGE_LIST, index=LANGUAGE_LIST)
+    for lang_train in LANGUAGE_LIST:
+        for lang_test in LANGUAGE_LIST:
+            train_input_file_path = BASE_DATA_FILES_ADDRESS + lang_train + '_train.csv'
+            test_input_file_path  = BASE_DATA_FILES_ADDRESS + lang_test  + '_test.csv'
+            output_file_path      = BASE_DATA_FILES_ADDRESS + lang_test  + '_model.txt'
+            lm(train_input_file_path, output_file_path)
+            preplexity = eval(test_input_file_path, output_file_path, WEIGHTS_LIST)
+            results[lang_test][lang_train] = preplexity
+    print (results)
 
-    # Extract weights from weights list
-    lambda_3_gram = weights[2]
-    lambda_2_gram = weights[1]
-    lambda_1_gram = weights[0]
-
-    # Create list of classes for each n-gram
-    # Each element in list should contain: n-chars, Px, logPx
-    eval_m.create_3_gram_matrix(model_file)
-    eval_m.create_2_gram_matrix(model_file)
-    eval_m.create_1_gram_matrix(model_file)
-
-    Accum_entropy = 0.0
-    for el_3_gram in eval_m.m_3_gram:
-        P_interp = lambda_3_gram * el_3_gram.Px + lambda_2_gram * eval_m.get2GramElement(el_3_gram.w_i, el_3_gram.w_im1).Px + lambda_1_gram * eval_m.get1GramElement(el_3_gram.w_i).Px
-        Accum_entropy -= P_interp*np.log(P_interp)
-
-    print ('Entropy = {}'.format(Accum_entropy))
-    print ('Perplexity = {}'.format(2**Accum_entropy))
 
 ###############################
 
-corpus_file = '/Users/guygozlan/Documents/Private/Binthomi/NLP/git/NLP/HW1/en_red.csv'
-model_file  = '/Users/guygozlan/Documents/Private/Binthomi/NLP/git/NLP/HW1/output_file_1.txt'
+run()
 
-lm(corpus_file, model_file)
-eval(corpus_file, model_file, [0.2,0.3,0.5])
